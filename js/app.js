10730b6aab98e2f676849b85adfed33ca0a4c289
Sha_code <shannone.controu@gmail.com>
2020-02-09 16:44:14 +0100

    erase storage when erase list + css

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 var app = {
 
-  addItems : document.querySelector('.add-items'),
-  itemsList : document.querySelector('.tasks'),
-  items : JSON.parse(localStorage.getItem('items')) || [],
-  eraseButton : document.querySelector('button'),
-  numberTodo : document.querySelector('.todo'),
+  addItems: document.querySelector('.add-items'),
+  itemsList: document.querySelector('.tasks'),
+  items: JSON.parse(localStorage.getItem('items')) || [],
+  eraseButton: document.querySelector('button'),
+  numberTodo: document.querySelector('.todo'),
 
-  init: function() {
-  app.addItems.addEventListener('submit', app.addItem);
-  app.itemsList.addEventListener('click', app.toggleDone);
-  app.eraseButton.addEventListener('click', app.eraseList)
-  app.populateList(app.items, app.itemsList);
+  init: function () {
+    app.addItems.addEventListener('submit', app.addItem);
+    app.itemsList.addEventListener('click', app.toggleDone);
+    app.eraseButton.addEventListener('click', app.eraseList)
+    app.populateList(app.items, app.itemsList);
   },
 
-  addItem: function(e) {
+  addItem: function (e) {
     e.preventDefault();
     const text = (this.querySelector('[name=item]')).value;
     const item = {
@@ -26,7 +26,7 @@ var app = {
     this.reset();
   },
 
-  populateList : function (tasks = [], tasksList) {
+  populateList: function (tasks = [], tasksList) {
     tasksList.innerHTML = tasks.map((task, i) => {
       return `
         <li>
@@ -35,24 +35,25 @@ var app = {
         </li>
       `;
     }).join('');
-    let todo = app.items.filter((task)=> {return !task.done}).length;
+    let todo = app.items.filter((task) => { return !task.done }).length;
     if (todo < 2) {
-      return app.numberTodo.innerHTML = `${todo} tâche en cours`    
+      return app.numberTodo.innerHTML = `${todo} tâche en cours`
     } else {
-     return app.numberTodo.innerHTML = `${todo} tâches en cours`
+      return app.numberTodo.innerHTML = `${todo} tâches en cours`
     }
   },
-  toggleDone : function (e) {
-    if (!e.target.matches('input')) return; // skip this unless it's an input
+  toggleDone: function (e) {
+    if (!e.target.matches('input')) return;
     const el = e.target;
     const index = el.dataset.index;
     app.items[index].done = !app.items[index].done;
     localStorage.setItem('items', JSON.stringify(app.items));
     app.populateList(app.items, app.itemsList);
   },
-  eraseList : function(){
+  eraseList: function () {
     app.items = [];
     app.populateList(app.items, app.itemsList);
+    localStorage.setItem('items', JSON.stringify(app.items));
   },
 };
 
